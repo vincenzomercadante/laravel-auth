581fc7f542bb5b3eb779259244c41cb496c09874
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-04-03 09:45:15 +0200

    feat: add delete & modify button with destroy method

--- resources/views/admin/project-details.blade.php
+++ resources/views/admin/project-details.blade.php
@@ -17,6 +17,17 @@
                 <h2><span class="fw-semibold text-capitalize mb-2 fs-4 d-inline-block me-2">Title:</span>{{$project->title}}</h2>
                 <h3><span class="fw-semibold text-capitalize mb-2 fs-4 d-inline-block">description: </span><br>{{$project->description}}</h3>
                 <a href="{{$project->github_reference}}">See on GitHub!</a>
+
+                <div class="d-flex gap-3 mt-3">
+                    <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary me-3 text-uppercase">Modify project!</a>
+                    <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        {{-- TODO: create the modal for the secure delete --}}
+                        <input type="button" value="delete project" class="btn btn-danger text-uppercase">
+                    </form>
+                </div>
             </div>
         </div>
     </div>
